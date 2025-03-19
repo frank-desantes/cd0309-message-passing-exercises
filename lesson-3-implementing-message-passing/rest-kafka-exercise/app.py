6d7cec079457e47ff1c9e4e8bbee2a7f2df9e23e
Frank Pohlen <frank.pohlen@telekom.de>
2025-03-19 15:03:39 +0100

    5.10 applying open api

--- lesson-3-implementing-message-passing/rest-kafka-exercise/app.py
+++ lesson-3-implementing-message-passing/rest-kafka-exercise/app.py
@@ -30,6 +30,20 @@ def computers():
     else:
         raise Exception('Unsupported HTTP request type.')
 
+@app.route('/api/v2/orders/computers', methods=['GET', 'POST'])
+def computersV2():
+    if request.method == 'GET':
+        return jsonify(retrieve_orders())
+    elif request.method == 'POST':
+        request_body = request.json
+        #print(request_body)
+        producer.send(TOPIC_NAME, bytes(str(request_body), 'utf-8'))
+        producer.flush()
+        #return jsonify(create_order(request_body))
+        return Response(status=202)
+    else:
+        raise Exception('Unsupported HTTP request type.')
+
 
 if __name__ == '__main__':
     app.run()
