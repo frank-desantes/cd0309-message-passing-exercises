09237a72b93a77e0e093195268e497cd2d1b9c33
Frank Pohlen <frank.pohlen@telekom.de>
2025-03-19 13:30:55 +0100

    5.6 use rest with kafka done

---
+++ lesson-3-implementing-message-passing/rest-kafka-exercise/app.py
@@ -0,0 +1,35 @@
+import json
+from flask import Flask, jsonify, request
+from kafka import KafkaProducer
+
+from .services import retrieve_orders, create_order
+
+TOPIC_NAME = 'computers'
+KAFKA_SERVER = 'localhost:9092'
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+
+app = Flask(__name__)
+
+
+@app.route('/health')
+def health():
+    return jsonify({'response': 'Hello World!'})
+
+
+@app.route('/api/orders/computers', methods=['GET', 'POST'])
+def computers():
+    if request.method == 'GET':
+        return jsonify(retrieve_orders())
+    elif request.method == 'POST':
+        request_body = request.json
+        print(request_body)
+        producer.send(TOPIC_NAME, bytes(str(request_body), 'utf-8'))
+        producer.flush()
+        return jsonify(create_order(request_body))
+    else:
+        raise Exception('Unsupported HTTP request type.')
+
+
+if __name__ == '__main__':
+    app.run()
+
